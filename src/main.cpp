27109f53bbfff06f39d4a1e2422cbf7e91783675
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-17 22:59:17 +0300

    check events has been move to a separate func

--- src/main.cpp
+++ src/main.cpp
@@ -16,28 +16,40 @@ void init_grid(std::vector<std::vector<sf::RectangleShape>>& grid) {
 }
 
 void set_start_postion(std::vector<std::vector<sf::RectangleShape>>& grid) {
-    std::vector<std::pair<int, int>> start_pos = {{20, 13}, {20, 14}, {20, 15}, {20, 16}, {20, 17}};
+    std::vector<std::pair<int, int>> start_pos = {{28, 42}, {29, 42}, {30, 42}, {31, 42}, {32, 42}};    // x/y format
     for (const auto& s : start_pos) {
         grid[s.second][s.first].setFillColor(sf::Color::Cyan);
+    }
+}
+
+void check_event_close(sf::RenderWindow& window) {
+    while (const std::optional event = window.pollEvent())
+    {
+        if (event->is<sf::Event::Closed>()) {
+            window.close();
+        }
+        else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+            if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                window.close();
+        }
     }
 }
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({width, height}), "Game of Life 2.0", sf::Style::None);
-    window.setFramerateLimit(30);
+    window.setFramerateLimit(60);
 
     std::vector<std::vector<sf::RectangleShape>> grid(y_cells, std::vector<sf::RectangleShape>(x_cells));
     init_grid(grid);
     set_start_postion(grid);
 
+    std::vector<std::vector<bool>> next_grid(y_cells, std::vector<bool>(x_cells));
+
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
+        check_event_close(window);
+
         window.clear();
         for (const auto& s : grid) {
             for (const auto& j : s) {
