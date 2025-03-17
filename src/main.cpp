bf7f20e58794df75071df871b38a1119296af6dd
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-18 00:38:17 +0300

    useless actions removed

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void init_grid(std::vector<std::vector<sf::RectangleShape>>& grid) {
     }
 }
 
-void set_start_postion(std::vector<std::vector<sf::RectangleShape>>& grid, std::vector<std::vector<bool>>& next_grid) {
+void set_start_postion(std::vector<std::vector<bool>>& next_grid) {
     std::vector<std::pair<int, int>> start_pos = {
         {26, 44}, {26, 45}, {27, 44}, {27, 45}, // Центральный блок
         {26, 52}, {27, 52}, {28, 52}, {25, 53}, {29, 53}, {24, 54}, {30, 54},
@@ -23,10 +23,6 @@ void set_start_postion(std::vector<std::vector<sf::RectangleShape>>& grid, std::
         {26, 62}, {25, 62}, {24, 62}, {26, 63}, {25, 63}, {24, 63}, {23, 64}, {27, 64}, {23, 66}, {27, 66},
         {28, 66}, {22, 68}, {23, 68}, {27, 68}, {28, 68}, {24, 78}, {25, 78}, {24, 79}, {25, 79}
     };
-
-    for (const auto& s : start_pos) {
-        grid[s.first][s.second].setFillColor(sf::Color::Cyan);
-    }
 
     for (const auto& s : start_pos) {
         next_grid[s.first][s.second] = true;
@@ -120,7 +116,7 @@ int main()
     std::vector<std::vector<sf::RectangleShape>> grid(y_cells, std::vector<sf::RectangleShape>(x_cells));
     init_grid(grid);
     std::vector<std::vector<bool>> next_grid(y_cells, std::vector<bool>(x_cells));
-    set_start_postion(grid, next_grid);
+    set_start_postion(next_grid);
 
     sf::Clock clock;
     constexpr sf::Time delay = sf::seconds(0.05f);
