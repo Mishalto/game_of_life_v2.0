06e9738e5b882e548ab1eaa7434298a125cb5ce0
Mishalto <167351152+Mishalto@users.noreply.github.com>
2025-03-17 22:58:25 +0300

    cell size setted to 20p

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -3,6 +3,6 @@
 constexpr int width = 1920;
 constexpr int height = 1080;
 
-constexpr int cell_size = 40;
+constexpr int cell_size = 20;
 constexpr int x_cells = width / cell_size;
 constexpr int y_cells = height / cell_size;
